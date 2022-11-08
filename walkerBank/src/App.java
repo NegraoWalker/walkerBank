4fdd5f05721d53035f5fac07f707c2d5a5e4ab9d
walkerNegrão <walker.esteves.negrao@gmail.com>
2022-11-07 22:46:01 -0300

    Primeiro método

--- walkerBank/src/App.java
+++ walkerBank/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
         Conta conta1 = new Conta();
-        conta1.saldo = 1000;
+        conta1.depositar(1000);
         // Exibindo na tela o valor da saldo da conta:
         System.out.println(conta1.saldo);
     }
--- walkerBank/src/Conta.java
+++ walkerBank/src/Conta.java
@@ -1,6 +1,12 @@
 public class Conta {
+    // Atributos da classe:
     double saldo;
     int agencia;
     int numero;
     String titular;
+
+    // Métodos da classe:
+    void depositar(double valor) {
+        saldo += valor;
+    }
 }

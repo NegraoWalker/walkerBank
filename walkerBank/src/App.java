e3407624cdddee5917500db9ee46aa8e48bb7e67
walkerNegrão <walker.esteves.negrao@gmail.com>
2022-11-07 21:04:38 -0300

    Instânciando objetos da classe Conta

--- walkerBank/src/App.java
+++ walkerBank/src/App.java
@@ -1,5 +1,8 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Conta conta1 = new Conta();
+        conta1.saldo = 1000;
+        // Exibindo na tela o valor da saldo da conta:
+        System.out.println(conta1.saldo);
     }
 }

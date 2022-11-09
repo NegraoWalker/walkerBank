121eedadfa3e172b07044058031c25137cbfb090
walkerNegrão <walker.esteves.negrao@gmail.com>
2022-11-08 22:45:53 -0300

    Atualização da aplicação

---
+++ walkerBank/src/Cliente.java
@@ -0,0 +1,9 @@
+public class Cliente {
+    // Atributos da classe:
+    String nome;
+    String cpf;
+    String profissao;
+
+    // Métodos da classe:
+
+}
--- walkerBank/src/Conta.java
+++ walkerBank/src/Conta.java
@@ -3,10 +3,24 @@ public class Conta {
     double saldo;
     int agencia;
     int numero;
-    String titular;
+    Cliente titular; // Composição do objeto da classe Cliente
 
     // Métodos da classe:
-    void depositar(double valor) {
-        saldo += valor;
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    public boolean sacar(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transferir(Conta nomeConta, double valor) {
+        nomeConta.sacar(valor);
+        this.depositar(valor);
+        return true;
     }
 }
